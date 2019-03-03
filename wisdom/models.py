9cb682a555eb23a60442ce1ef5844f0453a01bf2
anitejngo <laogdo@gmail.com>
2019-03-03 16:46:36 +0100

    Add str function for wisdom model

--- wisdom/models.py
+++ wisdom/models.py
@@ -5,3 +5,6 @@ class Wisdom(models.Model):
     words_of_wisdom = models.CharField(max_length=255)
     wisdom_origin = models.CharField(max_length=120)
     wisdom_occurrence_time = models.DateField()
+
+    def __str__(self):
+        return 'Wisdom: ' + self.words_of_wisdom

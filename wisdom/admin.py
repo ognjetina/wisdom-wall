3937f6474acb4de3f83ee47fb8154cbb0a3f2a2b
anitejngo <laogdo@gmail.com>
2019-03-03 15:04:27 +0100

    Add procfile

--- wisdom/admin.py
+++ wisdom/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Wisdom
 
 # Register your models here.
+admin.site.register(Wisdom)
--- wisdom/models.py
+++ wisdom/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Wisdom(models.Model):
+    words_of_wisdom = models.CharField(max_length=255)
+    wisdom_origin = models.CharField(max_length=120)
+    wisdom_occurrence_time = models.DateField()

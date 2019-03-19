5d680aea845eec718ede7de833bc58923b735e97
Bruno Raljic <brunoraljic@yahoo.com>
2019-03-19 23:06:28 +0100

    Implement fetching wisdom by ID

--- wisdom/views.py
+++ wisdom/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+
 from .models import Wisdom
 
 
 def random_wisdom(request):
-    random_wisdom = Wisdom.objects.order_by('?').first()
-    return render(request, 'home.html', {'random_wisdom': random_wisdom})
+    some_random_wisdom = Wisdom.objects.order_by('?').first()
+    return render(request, 'home.html', {'wisdom': some_random_wisdom})
+
+
+def wisdom_by_id(request, wisdom_id):
+    wisdom = Wisdom.objects.filter(id=wisdom_id).first()
+    if not wisdom:
+        return random_wisdom(request)
+    return render(request, 'home.html', {'wisdom': wisdom})

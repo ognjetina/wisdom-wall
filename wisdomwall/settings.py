0082b5df2bd3b7eb5f0f4082d232b5ac2bd1e28c
anitejngo <laogdo@gmail.com>
2019-12-13 10:16:29 +0100

    Remove settings for templates

--- wisdomwall/settings.py
+++ wisdomwall/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'wisdomwall.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'wisdom', 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

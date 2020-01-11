a7081215e451d1fafb95434d177e029abbb29015
anitejngo <laogdo@gmail.com>
2020-01-11 16:45:52 +0100

    Change twitter war to twitter leaderboard

--- twitterwar/views.py
+++ twitterwar/views.py
@@ -8,9 +8,16 @@ def twitter_war(request):
                         '2982752483-A6yeIrm7qC4QHPL3Ar0Hphvg2bZHqHAnJ62f7sr',
                         'o8qR72i1tiampZnWtlZOuZq97tyEBynNk75U9BhH5X2r3')
 
-    bruno = client.api.users.show.get(screen_name='brunoraljic')
-    enso = client.api.users.show.get(screen_name='EnsarBavrk')
+    twitter_names = ['brunoraljic', 'EnsarBavrk', "Ognjetina", "sdejan89", "_rstokic_",
+                     "milansusnjar_", "KnightOfRen_", "A_Popadic", "milos_brdar", "borisjanjanin",
+                     "deni_n88", "djovic82", "GJungic", "Gonguli88", "buconis", "ozegster", "mladen_vasic",
+                     "Nemanjas_Vasic", "novislavs", "stek993", "Tin_M_", "stasa_m"]
 
-    return render(request, 'war.html',
-                  {"bruno_followers": bruno.data['followers_count'], "enso_followers": enso.data['followers_count'],
-                   'bruno_image': bruno.data['profile_image_url'], 'enso_image': enso.data['profile_image_url']})
+    twitter_accounts = []
+
+    for twitter_name in twitter_names:
+        twitter_accounts.append(client.api.users.show.get(screen_name=twitter_name).data)
+
+    twitter_accounts.sort(key=lambda x: x['followers_count'], reverse=True)
+
+    return render(request, 'war.html', {"twitter_accounts": twitter_accounts[:10]})
